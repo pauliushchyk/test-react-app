b3ce3e50f78c069f305c2e716346d9f0b1d5e43c
Pavel Pauliushchyk <ppauliushchyk@gmail.com>
2018-11-13 13:02:06 +0300

    feat: added background for card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -10,10 +10,12 @@ export const Card = () => (
         'url(https://content.kufar.by/line_thumbs_2x/68/6840991619.jpg)',
     }}
   >
-    <h2 className="card__title">Title</h2>
-    <div className="card__prices">
-      <span className="card__price">120 809р</span>
-      <span className="card__price card__price--small">5 400$</span>
+    <div className="card__content">
+      <h2 className="card__title">Title</h2>
+      <div className="card__prices">
+        <span className="card__price">120 809р</span>
+        <span className="card__price card__price--small">5 400$</span>
+      </div>
     </div>
   </div>
 );
